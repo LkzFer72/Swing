7c50f4108bce288487a09f0304c786fa6ccd5eae
LkzFer72 <Acxel271212@gmail.com>
2025-05-09 14:53:51 -0700

    práctica jueves/viernes

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -1,10 +1,34 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginForm {
+public class LoginForm extends Component {
     private JPanel panelPrincipal;
     private JTextField usuarioTexto;
     private JPasswordField passwordTexto;
     private JButton enviarButton;
+
+    public LoginForm() {
+        enviarButton.addActionListener(e -> validar());
+        enviarButton.addActionListener(e -> validar());
+    }
+
+    private void validar() {
+        // Leer los valores de los campos de texto
+        String usuario = this.usuarioTexto.getText();
+        String password = new String(passwordTexto.getPassword());
+        if (usuario.equals("admin") && password.equals("1234")) {
+            JOptionPane.showMessageDialog(this, "Usuario correcto, bienvenido", "Datos correctos", JOptionPane.INFORMATION_MESSAGE);
+        } else if (usuario.equals("admin")) {
+            JOptionPane.showMessageDialog(this, "Password incorrecto, corrigelo", "Datos Incorrectos", JOptionPane.WARNING_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(this, "Usuario incorrecto, corrigelo", "Datos Incorrectos", JOptionPane.ERROR_MESSAGE);
+
+        }
+
+
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("LoginForm");
@@ -12,7 +36,7 @@ public class LoginForm {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
 
-        frame.setSize(400,300);
+        frame.setSize(400, 300);
         frame.setLocationRelativeTo(null);
 
         frame.setVisible(true);
